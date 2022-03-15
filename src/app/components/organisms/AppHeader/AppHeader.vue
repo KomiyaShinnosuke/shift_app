94c604840976fd3f4ef4fe9f933f32180788e639
KomiyaShinnosuke <shinnosuke828@gmail.com>
2022-03-16 08:00:53 +0900

    feat: グローバルヘッダーの整理

--- src/app/components/organisms/AppHeader/AppHeader.vue
+++ src/app/components/organisms/AppHeader/AppHeader.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="app-header">
+    <ul class="menus">
+      <li class="menu" @click="handleClickCalendar">
+        <Icon icon="mdi-calendar-blank" /><v-btn class="button">カレンダー</v-btn>
+      </li>
+      <li class="menu">
+        <Icon icon="mdi-calendar-edit" /><v-btn class="button">シフト作成</v-btn>
+      </li>
+    </ul>
     <Icon icon="mdi-cog"  @click="handleClickSetting" />
   </div>
 </template>
@@ -7,16 +15,21 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
+import { Button } from '@/components/atoms/Button'
 import { Icon } from '@/components/atoms/Icon'
 
 export default defineComponent({
-  components: { Icon },
+  components: { Button, Icon },
   setup() {
     const router = useRouter()
+    const handleClickCalendar = () => {
+      router.push({ name: 'MonthlyCalendar' })
+    }
     const handleClickSetting = () => {
       router.push({ name: 'Setting' })
     }
     return {
+      handleClickCalendar,
       handleClickSetting,
     }
   },
@@ -25,5 +38,21 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .app-header {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 40px;
+  width: 100%;
+  .menus {
+    display: flex;
+    justify-content: space-between;
+    width: 25%;
+    .menu {
+      display: flex;
+      .button {
+        height: 24px;
+        padding-left: 4px;
+      }
+    }
+  }
 }
 </style>
