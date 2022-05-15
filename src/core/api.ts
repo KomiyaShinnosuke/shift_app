f89dc9d37cd43301d86015151fdafd7e1606344b
KomiyaShinnosuke <shinnosuke828@gmail.com>
2022-05-15 15:59:05 +0900

    fix: API通信がタイムアウトしてしまうのを対応

--- src/core/api.ts
+++ src/core/api.ts
@@ -5,14 +5,8 @@ axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 const API_URL = '/api/v1'
 const client = axios.create({
   baseURL: API_URL,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, HEAD, OPTIONS',
-  //   'Access-Control-Allow-Headers': "X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept",
-  // },
   responseType: 'json',
-  timeout: 3000,
+  timeout: 30000,
 })
 
 export default client
